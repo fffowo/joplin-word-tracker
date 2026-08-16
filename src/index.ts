fe6a8119a21a414da7594c907fe6c16ff7d088da
fo <fowo@tutanota.com>
2026-08-16 16:12:46 +0200

    add tracker to panel

--- src/index.ts
+++ src/index.ts
@@ -14,12 +14,38 @@ joplin.plugins.register({
 
             // Keep in mind that it can be `null` if nothing is currently selected!
             if (note) {
-                console.info('Note content has changed! New note is:', note);
+                // console.info('Note content has changed! New note is:', note);
 
-				const wordCount = note ? note.body.split(/\s+/).length : 0;
-				console.info('Word count:', wordCount);
-
+				const body = note ? note.body.trim().escapeHtml() : 0;
+				console.log('Note body:', body); // check the actual unformatted body 
 				
+				// TODO: formatting  
+				// const words = body.split([' ']); // returns array 
+				const words = body.split(/\s+/); // splits all whitespaces
+
+
+				let counter = words.length; 
+
+				console.log('Words:', counter);
+				
+
+
+				// TODO 
+		        // Create the panel object
+				const panel = await joplin.views.panels.create('panel_wordcount');
+				let itemHtml = [];
+				itemHtml.push(`
+                        <p>
+								Words: <b>${counter}</b>
+                        </p>
+                    `);
+
+                // Finally, insert all the headers in a container and set the webview HTML:
+                await joplin.views.panels.setHtml(panel, `
+                    <div class="container">
+                        ${itemHtml.join('\n')}
+                    </div>
+                `);
 
             } else {
                 console.info('No note is selected');
@@ -42,3 +68,13 @@ joplin.plugins.register({
     },
 
 });
+
+// From https://stackoverflow.com/a/6234804/561309
+function escapeHtml(unsafe:string) {
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
